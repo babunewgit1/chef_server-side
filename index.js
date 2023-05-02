d00c4568b13f32091847fc5ee1ff2373f6530df4
babunewgit1 <babunewx11@gmail.com>
2023-05-02 14:12:32 +0600

    server setup completed

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const app = express();
+const port = 5000;
+const cors = require("cors");
+app.use(cors());
+
+const main = require("./data/main.json");
+const feedback = require("./data/feedback.json");
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.get("/main", (req, res) => {
+  res.send(main);
+});
+
+app.get("/feedback", (req, res) => {
+  res.send(feedback);
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
